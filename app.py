4183922c68f521b79634dd6df90c3b5b64d3a81a
shakedva <shakediii123@gmail.com>
2023-06-08 21:13:27 +0300

    Web API status wip

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ from werkzeug.utils import secure_filename
 import os
 import uuid
 from datetime import datetime
-
-UPLOAD_FOLDER = "upload"
+from pathlib import Path
+from enum import Enum
+import json
+UPLOAD_DIR_NAME = "upload"
 ALLOWED_EXTENSIONS = {'pptx'}
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['UPLOAD_FOLDER_PATH'] = Path(Path.cwd() / UPLOAD_DIR_NAME)
 
 
 def allowed_file(filename: str):
@@ -17,8 +19,12 @@ def allowed_file(filename: str):
 
 
 def save_file(file, file_name, uid):
-    data = (datetime.today().strftime('%Y-%m-%d'), uid, file_name)
-    file.save(os.path.join(app.config['UPLOAD_FOLDER'], '_'.join(data)))
+    directory_path = app.config['UPLOAD_FOLDER_PATH'] / uid
+    directory_path.mkdir(exist_ok=True)
+
+    data = (datetime.now().strftime('%Y-%m-%d-%H-%M-%S'), uid, file_name)
+    file_name = '_'.join(data)
+    file.save(directory_path / file_name)
 
 
 @app.route('/', methods=['POST'])
@@ -35,3 +41,36 @@ def upload_file():
         uid = str(uuid.uuid1())
         save_file(file, file_name, uid)
         return jsonify(uid=uid)
+
+
+class Status(str, Enum):
+    DONE = 'done'
+    PENDING = 'pending'
+    NOT_FOUND = 'not found'
+
+
+@app.route('/<uid>', methods=['GET'])
+def status(uid):
+    response = {
+        'explanation': None
+    }
+
+    output_dir = app.config['UPLOAD_FOLDER_PATH'] / uid
+    if output_dir.is_dir():
+        output_dir_files = list(output_dir.glob("*.json"))
+
+        if output_dir_files:
+            response['status'] = Status.DONE
+            with open(output_dir_files[0], 'r') as f:
+                response['explanation'] = json.loads(f.read())
+        else:
+            response['status'] = Status.PENDING
+
+        file_name = next(output_dir.glob("*.pptx"))
+        if file_name:
+            timestamp, _, original_file_name = file_name.name.split('_', 2)
+            response['filename'] = original_file_name
+            response['timestamp'] = timestamp
+    else:
+        response['status'] = Status.NOT_FOUND
+    return jsonify(response)
