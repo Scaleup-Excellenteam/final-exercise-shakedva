8898334bd056a29a12864690edaedd1a44262e11
shakedva <shakediii123@gmail.com>
2023-06-27 17:59:06 +0300

    Fixed db path

--- db/orm.py
+++ db/orm.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
@@ -5,12 +6,10 @@ from sqlalchemy.orm import relationship
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import ForeignKey
 from api.request_status_enum import RequestStatusEnum
-import sqlalchemy  # import Enum
+import sqlalchemy
 from contextlib import contextmanager
-
 from typing import List
-from sqlalchemy import Integer, String, UUID, DateTime
-from sqlalchemy import create_engine
+from sqlalchemy import Integer, String, DateTime, create_engine, CheckConstraint
 
 
 class Base(DeclarativeBase):
@@ -21,25 +20,24 @@ class User(Base):
     __tablename__ = "user"
     id = mapped_column(Integer, primary_key=True)
     email = mapped_column(String, nullable=False, unique=True)
-
     uploads: Mapped[List["Upload"]] = relationship(
         back_populates="user", cascade="all, delete-orphan"
     )
-
-    def __repr__(self) -> str:
-        return f"User(id={self.id!r}, email={self.email!r})"
 
 
 class Upload(Base):
     __tablename__ = "upload"
     id = mapped_column(Integer, primary_key=True)
-    uid = mapped_column(String)  # TODO
+    uid = mapped_column(String)
     filename = mapped_column(String, nullable=False)
     upload_time = mapped_column(DateTime)
     finish_time = mapped_column(DateTime)
-    status = mapped_column(sqlalchemy.Enum(RequestStatusEnum))  # TODO - does not work as expected
+    status = mapped_column(sqlalchemy.Enum(RequestStatusEnum))
     user_id = mapped_column(Integer, ForeignKey("user.id"))
     user: Mapped["User"] = relationship(back_populates="uploads")
+    __table_args__ = (
+        CheckConstraint(status.in_(RequestStatusEnum.__members__.values())),
+    )
 
 
 class Singleton(type):
@@ -58,12 +56,15 @@ class DB(metaclass=Singleton):
     """
     Database singleton class to manage the operations.
     """
+
     def __init__(self):  # TODO
         """
         Initialize the database
         """
+        db_path = Path(Path.cwd() / 'db' / "final_exercise.db")
         self.engine = engine = create_engine(
-            "sqlite:///C:\\Users\\shake\\Desktop\\College\\4th Year\\Semester B\\Excellenteam\\python\\Ex\\final-exercise-shakedva\\db\\example.db"
+
+            "sqlite:///" + str(db_path)
         )
         Base.metadata.create_all(bind=engine)
 
